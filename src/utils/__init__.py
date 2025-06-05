839f1fdde0bb6bb04181123c26d6b7a14421f5fc
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2025-06-05 14:49:01 +0800

    add json utils

---
+++ src/utils/__init__.py
@@ -0,0 +1,6 @@
+from .json_util import load_json, load_jsonl
+
+__all__ = [
+    "load_jsonl",
+    "load_json",
+]
---
+++ src/utils/json_util.py
@@ -0,0 +1,32 @@
+import json
+
+
+def load_jsonl(file_path: str) -> list[dict]:
+    """
+    Load a JSON Lines file and return a list of dictionaries.
+
+    Args:
+        file_path (str): The path to the JSON Lines file.
+
+    Returns:
+        list[dict]: A list of dictionaries loaded from the file.
+    """
+    data = []
+    with open(file_path, encoding="utf-8") as file:
+        for line in file:
+            data.append(json.loads(line))
+    return data
+
+
+def load_json(file_path: str) -> dict:
+    """
+    Load a JSON file and return its content as a dictionary.
+
+    Args:
+        file_path (str): The path to the JSON file.
+
+    Returns:
+        dict: The content of the JSON file as a dictionary.
+    """
+    with open(file_path, encoding="utf-8") as file:
+        return json.load(file)
